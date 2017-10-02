377a785fb26fc612d8f456cd08d48bbd8ee4e1a5
mmarcec007 <mark.marcec@gmail.com>
2017-10-02 14:17:11 +0200

    [TASK] map Jamanete to alphabet

--- app/src/main/java/com/example/mark/jnames/datasource/LettersConverter.java
+++ app/src/main/java/com/example/mark/jnames/datasource/LettersConverter.java
@@ -11,10 +11,34 @@ public class LettersConverter {
     Map<String, String> map = new HashMap<String, String>();
 
     public LettersConverter(Map<String, String> map) {
-        map.put("Color1","Red");
-        map.put("Color2","Blue");
-        map.put("Color3","Green");
-        map.put("Color4","White");
+        map.put("A","KA");
+        map.put("B","ZU");
+        map.put("C","Mi");
+        map.put("D","TE");
+        map.put("E","KU");
+        map.put("F","LU");
+        map.put("G","Ji");
+        map.put("H","Ri");
+        map.put("I","Ki");
+        map.put("J","ZU");
+        map.put("K","ME");
+        map.put("L","TA");
+        map.put("M","RiN");
+        map.put("N","TO");
+        map.put("O","MO");
+        map.put("P","NO");
+        map.put("Q","KE");
+        map.put("R","SHi");
+        map.put("S","ARi");
+        map.put("T","CHi");
+        map.put("U","DO");
+        map.put("V","RU");
+        map.put("W","MEi");
+        map.put("X","NA");
+        map.put("Y","FU");
+        map.put("Z","Z");
+
+
         this.map = map;
     }
 }
