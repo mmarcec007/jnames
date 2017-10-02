453f30582cbce523397f7a3b7b255bfee592404b
mmarcec007 <mark.marcec@gmail.com>
2017-10-02 14:45:51 +0200

    [TASK] add onChangeText listener

--- app/src/main/java/com/example/mark/jnames/MainActivity.java
+++ app/src/main/java/com/example/mark/jnames/MainActivity.java
@@ -1,13 +1,41 @@
 package com.example.mark.jnames;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText inputName = (EditText)findViewById(R.id.input_name);
+        final TextView resultName = (TextView)findViewById(R.id.result_name);
+
+        inputName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                resultName.setText(inputName.getText().toString());
+            }
+        });
     }
 }
